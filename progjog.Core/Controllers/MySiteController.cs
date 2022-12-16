90cc9f0ad925356fe1d320a8d93cb11ceb8d345a
Kent Martin Åsvang <kent@asvang.no>
2022-12-16 23:53:00 +0100

    some plumbling

---
+++ progjog.Core/Controllers/MySiteController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using progjog.Core.Models;
+
+namespace progjog.Core.Controllers;
+
+[Authorize]
+public class MySiteController : Controller
+{
+    private readonly ILogger<MySiteController> _logger;
+
+    public MySiteController(ILogger<MySiteController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult CreateTrainingProgram()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+}
