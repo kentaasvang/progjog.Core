a38e03dc38e551c99795437361f683afc5e3edad
Kent Martin Åsvang <kent@asvang.no>
2022-12-18 00:29:40 +0100

    verified InMemoryIntegrationTest is working

--- progjog.Web.IntegrationTests/HomeControllerTest.cs
+++ progjog.Web.IntegrationTests/HomeControllerTest.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using progjog.Web.Data;
+using progjog.Web.Data.Entities;
 using Xunit;
 
 namespace progjog.Web.IntegrationTests;
@@ -11,17 +16,19 @@ public class HomeControllerTest : IClassFixture<CustomWebApplicationFactory<Prog
     {
         _factory = factory;
     }
-    
+
     [Fact]
     public async Task Get_ShouldReturnSuccessAndCorrectContentType()
     {
         // Arrange
         var client = _factory.CreateClient();
-        
+
         // Act
         var response = await client.GetAsync("/");
 
         // Assert
         response.EnsureSuccessStatusCode();
+        Assert.Equal("text/html; charset=utf-8", 
+            response.Content.Headers.ContentType?.ToString());
     }
 }
