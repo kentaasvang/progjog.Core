e03dffcf79868b416e7f38cf8652ae5d2161a35a
Kent Martin Åsvang <kent@asvang.no>
2022-12-17 17:27:02 +0100

    refactored and moved around configuration of mapster

--- progjog.Web/MapsterConfiguration.cs
+++ progjog.Web/MapsterConfiguration.cs
@@ -1,27 +1,30 @@
-
 using Mapster;
-using MapsterMapper;
+using progjog.Web.Data.Entities;
+using progjog.Web.Models;
 
 namespace progjog.Web;
 
-public static class AddMapsterExtensions
+public static class MapsterConfiguration
 {
-    public static void AddMapster(this IServiceCollection services)
-    {
-        var config = GetConfiguration();
-
-        services.AddSingleton(config);
-        services.AddTransient<IMapper, ServiceMapper>();
-    }
-
-    private static TypeAdapterConfig GetConfiguration()
+    public static TypeAdapterConfig GetConfiguration()
     {
         var config = new TypeAdapterConfig
         {
             RequireExplicitMapping = true,
             RequireDestinationMemberSource = true
         };
+
+        config.AddMappings();
+        
         config.Compile();
+        
         return config;
+    }
+
+    private static void AddMappings(this TypeAdapterConfig config)
+    {
+        config.NewConfig<TrainingPlan, TrainingPlanViewModel>()
+            .Map(dest => dest.Owner, source => source.Owner.UserName)
+            .Map(dest => dest.Weeks, source => source.Duration);
     }
 }
